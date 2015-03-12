9b246d5c7315f37aa63701bd8c4609d2be06cdc0
Christopher Read <inter8ection@hotmail.com>
2015-03-12 19:43:05 +0000

    First commit

---
+++ Obvs.Serialization/DeserializerBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Obvs.Serialization
+{
+    public abstract class DeserializerBase<TMessage>
+    {
+        public string GetTypeName()
+        {
+            return typeof(TMessage).Name;
+        }
+    }
+}
---
+++ Obvs.Serialization/XmlMessageSerializer.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Obvs.Serialization
+{
+    public class XmlMessageSerializer<TMessage> : IMessageSerializer
+    {
+        private readonly XmlSerializer _xmlSerializer;
+
+        public XmlMessageSerializer()
+        {
+            _xmlSerializer = new XmlSerializer(typeof(TMessage));
+        }
+
+        public object Serialize(object obj)
+        {
+            using (StringWriter writer = new StringWriter())
+            {
+                _xmlSerializer.Serialize(writer, obj);
+                return writer.ToString();
+            }
+        }
+    }
+}
