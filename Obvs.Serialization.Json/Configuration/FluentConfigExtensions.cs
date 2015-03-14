c4fad36b9da13f0c647f7da8dc341439a1fbe74d
Christopher Read <inter8ection@hotmail.com>
2015-03-14 10:31:13 +0000

    Added fluent config extension points

---
+++ Obvs.Serialization.Json/Configuration/FluentConfigExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Obvs.Configuration;
+
+namespace Obvs.Serialization.Json.Configuration
+{
+    public static class FluentConfigExtensions
+    {
+        public static ICanCreateEndpointAsClientOrServer SerializedAsJson(this ICanSpecifyEndpointSerializers config)
+        {
+            return config.SerializedWith(new JsonMessageSerializer(), new JsonMessageDeserializerFactory());
+        }
+    }
+}
---
+++ Obvs.Serialization.ProtoBuf/Configuration/FluentConfigExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Obvs.Configuration;
+
+namespace Obvs.Serialization.ProtoBuf.Configuration
+{
+    public static class FluentConfigExtensions
+    {
+        public static ICanCreateEndpointAsClientOrServer SerializedAsProtoBuf(this ICanSpecifyEndpointSerializers config)
+        {
+            return config.SerializedWith(new ProtoBufMessageSerializer(), new ProtoBufMessageDeserializerFactory());
+        }
+    }
+}
