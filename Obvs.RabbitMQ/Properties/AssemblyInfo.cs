7482015d7ff7e4cce197c5c591346f25447ca79d
Christopher Read <inter8ection@hotmail.com>
2015-03-19 22:49:00 +0000

    Add author to assembly properties

--- Obvs.RabbitMQ/Properties/AssemblyInfo.cs
+++ Obvs.RabbitMQ/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Obvs.RabbitMQ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Christopher Read")]
 [assembly: AssemblyProduct("Obvs.RabbitMQ")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
