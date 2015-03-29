a178c774fd26dd67a1f57f5b4b811b0c99ddaa7e
Christopher Read <inter8ection@hotmail.com>
2015-03-29 14:16:35 +0100

    Catch and rethrow exception with more useful message when attempting to load types from assemblies

--- Obvs/Configuration/MessageTypes.cs
+++ Obvs/Configuration/MessageTypes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Obvs.Types;
 
 namespace Obvs.Configuration
@@ -13,12 +14,39 @@ namespace Obvs.Configuration
         {
             return AppDomain.CurrentDomain
                 .GetAssemblies()
-                .Where(assembly => string.IsNullOrEmpty(assemblyNameContains) || assembly.FullName.Contains(assemblyNameContains))
-                .SelectMany(assembly => assembly.GetTypes())
-                .Where(type => typeof(TMessage).IsAssignableFrom(type) &&
-                               typeof(TServiceMessage).IsAssignableFrom(type) &&
-                               type.IsClass &&
-                               type.IsVisible);
+                .Where(assembly => MatchesFilter(assemblyNameContains, assembly))
+                .SelectMany(GetTypes)
+                .Where(IsValidMessageType<TMessage, TServiceMessage>)
+                .ToArray();
+        }
+
+        private static bool MatchesFilter(string assemblyNameContains, Assembly assembly)
+        {
+            return string.IsNullOrEmpty(assemblyNameContains) || assembly.FullName.Contains(assemblyNameContains);
+        }
+
+        private static bool IsValidMessageType<TMessage, TServiceMessage>(Type type) 
+            where TMessage : IMessage 
+            where TServiceMessage : IMessage
+        {
+            return typeof(TMessage).IsAssignableFrom(type) &&
+                   typeof(TServiceMessage).IsAssignableFrom(type) &&
+                   type.IsClass &&
+                   type.IsVisible;
+        }
+
+        private static Type[] GetTypes(Assembly assembly)
+        {
+            try
+            {
+                return assembly.GetTypes();
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(string.Format(
+                        "Error loading types from assembly '{0}', please consider using the provided configuration options to filter which assemblies you would like to load your message types from.",
+                        assembly.FullName), exception);
+            }
         }
     }
 }
