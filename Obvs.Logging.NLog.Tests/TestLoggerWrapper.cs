8b0c6086f89b28a4fd3d6b2368d8f15f83f0b1cf
Christopher Read <inter8ection@hotmail.com>
2015-04-02 21:47:39 +0100

    Initial commit

---
+++ Obvs.Logging.NLog.Tests/TestLoggerWrapper.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace Obvs.Logging.NLog.Tests
+{
+    [TestFixture]
+    public class TestLoggerWrapper
+    {
+        [Test]
+        public void TestLoggerUsesCorrectName()
+        {
+            MyClass myClass = new MyClass(new NLogLoggerFactory());
+            myClass.LogSomethingBaby();
+        }
+    }
+
+    public class MyClass
+    {
+        private readonly ILogger _logger;
+
+        public MyClass(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.Create<MyClass>();
+        }
+
+        public void LogSomethingBaby()
+        {
+            _logger.Debug("Debug Message");
+            _logger.Info("Info Message");
+            _logger.Warn("Warn Message");
+            _logger.Error("Error Message");
+        }
+    }
+}
---
+++ Obvs.Logging.NLog/NLogLoggerWrapper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NLog;
+
+namespace Obvs.Logging.NLog
+{
+    public class NLogLoggerWrapper : ILogger
+    {
+        private readonly Logger _logger;
+
+        public NLogLoggerWrapper(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Debug(string message, Exception exception = null)
+        {
+            if (exception == null)
+            {
+                _logger.Debug(message);
+            }
+            else
+            {
+                _logger.DebugException(message, exception);
+            }
+        }
+
+        public void Info(string message, Exception exception = null)
+        {
+            if (exception == null)
+            {
+                _logger.Info(message);
+            }
+            else
+            {
+                _logger.InfoException(message, exception);
+            }
+        }
+
+        public void Warn(string message, Exception exception = null)
+        {
+            if (exception == null)
+            {
+                _logger.Warn(message);
+            }
+            else
+            {
+                _logger.WarnException(message, exception);
+            }
+        }
+
+        public void Error(string message, Exception exception = null)
+        {
+            if (exception == null)
+            {
+                _logger.Error(message);
+            }
+            else
+            {
+                _logger.ErrorException(message, exception);
+            }
+        }
+    }
+}
