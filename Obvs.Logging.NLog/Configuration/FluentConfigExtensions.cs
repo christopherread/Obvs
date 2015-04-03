b82c15f230a042fa7fcf6210794caa72c54e41a5
Christopher Read <christopher.read@starlizard.com>
2015-04-03 12:17:15 +0100

    Added fluent config extension

---
+++ Obvs.Logging.NLog/Configuration/FluentConfigExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Obvs.Configuration;
+
+namespace Obvs.Logging.NLog.Configuration
+{
+    public static class FluentConfigExtensions
+    {
+        public static ICanCreate UsingNLog(this ICanAddEndpointOrLoggingOrCreate configurator)
+        {
+            return configurator.UsingLogging(new NLogLoggerFactory());
+        }
+    }
+}
