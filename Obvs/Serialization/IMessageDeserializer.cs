0aa118c06d7388c91df226b2de0c906deb111f80
Christopher Read <inter8ection@hotmail.com>
2015-04-05 07:53:20 +0100

    Added overloads to serialization to use streams

--- Obvs/Serialization/IMessageDeserializer.cs
+++ Obvs/Serialization/IMessageDeserializer.cs
@@ -1,14 +1,18 @@
+using System.IO;
+
 namespace Obvs.Serialization
 {
     public interface IMessageDeserializer<out TMessage>
     {
         TMessage Deserialize(object obj);
+        TMessage Deserialize(Stream source);
         string GetTypeName();
     }
 
     public abstract class MessageDeserializerBase<TMessage> : IMessageDeserializer<TMessage>
     {
         public abstract TMessage Deserialize(object obj);
+        public abstract TMessage Deserialize(Stream source);
 
         public string GetTypeName()
         {
--- Obvs/Serialization/IMessageSerializer.cs
+++ Obvs/Serialization/IMessageSerializer.cs
@@ -1,7 +1,10 @@
+using System.IO;
+
 namespace Obvs.Serialization
 {
     public interface IMessageSerializer
     {
         object Serialize(object message);
+        void Serialize(Stream destination, object message);
     }
 }
