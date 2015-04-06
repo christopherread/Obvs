6b8f13f0632d1947bf41ece3937620ee4b47784f
Christopher Read <inter8ection@hotmail.com>
2015-04-06 20:48:33 +0100

    Upgrading to Obvs 1.5.2 with optional logging parameter

--- Obvs.Logging.NLog/Configuration/FluentConfigExtensions.cs
+++ Obvs.Logging.NLog/Configuration/FluentConfigExtensions.cs
@@ -1,12 +1,13 @@
-﻿using Obvs.Configuration;
+﻿using System;
+using Obvs.Configuration;
 
 namespace Obvs.Logging.NLog.Configuration
 {
     public static class FluentConfigExtensions
     {
-        public static ICanCreate UsingNLog(this ICanAddEndpointOrLoggingOrCreate configurator)
+        public static ICanCreate UsingNLog(this ICanAddEndpointOrLoggingOrCreate configurator, Func<IEndpoint, bool> enableLogging = null)
         {
-            return configurator.UsingLogging(new NLogLoggerFactory());
+            return configurator.UsingLogging(new NLogLoggerFactory(), enableLogging);
         }
     }
 }
