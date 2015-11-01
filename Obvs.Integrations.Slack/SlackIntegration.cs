6ce649ff81f6c0e3b97e2d6aeb942efc4b5e2c89
Christopher Read <inter8ection@hotmail.com>
2015-11-01 17:23:52 -0500

    Creating Obvs Integrations solution

---
+++ Obvs.Integrations.Slack/SlackIntegration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Obvs.Types;
+
+namespace Obvs.Integrations.Slack
+{
+    public class SlackIntegration : IServiceEndpointClient
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanHandle(IMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name { get; }
+        public Task SendAsync(ICommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<IResponse> GetResponses(IRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<IEvent> Events { get; private set; }
+    }
+}
