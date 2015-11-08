3b4838a75b2a9256f4772d9f85e91dbc91f82d2f
Christopher Read <inter8ection@hotmail.com>
2015-11-08 15:44:34 +0000

    Added the ability to add multiple monitors

---
+++ Obvs/Monitoring/MultiMonitor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Obvs.Monitoring
+{
+    public class MultiMonitor<TMessage> : IMonitor<TMessage>
+    {
+        private readonly IList<IMonitor<TMessage>> _monitors;
+
+        public MultiMonitor(IList<IMonitor<TMessage>> monitors)
+        {
+            _monitors = monitors;
+        }
+
+        public void Dispose()
+        {
+            foreach (var monitor in _monitors)
+            {
+                monitor.Dispose();
+            }
+        }
+
+        public void MessageSent(TMessage message, TimeSpan elapsed)
+        {
+            foreach (var monitor in _monitors)
+            {
+                monitor.MessageSent(message, elapsed);
+            }
+        }
+
+        public void MessageReceived(TMessage message, TimeSpan elapsed)
+        {
+            foreach (var monitor in _monitors)
+            {
+                monitor.MessageSent(message, elapsed);
+            }
+        }
+    }
+}
---
+++ Obvs/Monitoring/MultiMonitorFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Obvs.Monitoring
+{
+    public class MultiMonitorFactory<TMessage> : IMonitorFactory<TMessage>
+    {
+        private readonly IList<IMonitorFactory<TMessage>> _factories;
+
+        public MultiMonitorFactory(IList<IMonitorFactory<TMessage>> factories)
+        {
+            _factories = factories;
+        }
+
+        public IMonitor<TMessage> Create(string name)
+        {
+            return new MultiMonitor<TMessage>(_factories.Select(f => f.Create(name)).ToList());
+        }
+    }
+}
