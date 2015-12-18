84ee150321b2f73b681d6e83d7ab4f785d7c79af
James Connor <james.connor@starlizard.com>
2015-12-18 14:54:36 +0000

    Merges ScheduledSubjectMessageBus to SubjectMessageBus.

--- Obvs/SubjectMessageBus.cs
+++ Obvs/SubjectMessageBus.cs
@@ -9,13 +9,18 @@ namespace Obvs
     public class SubjectMessageBus<TMessage> : IMessageBus<TMessage>
         where TMessage : class
     {
-
-        // Could use ScheduledSubjectMessageBus<TMessage> with Scheduler.Immediate but this is simpler
         private readonly Subject<TMessage> _subject;
 
         public SubjectMessageBus()
+            : this(null)
+        {
+        }
+
+        public SubjectMessageBus(IScheduler scheduler)
         {
             _subject = new Subject<TMessage>();
+
+            Messages = scheduler == null ? _subject.AsObservable() : _subject.ObserveOn(scheduler).Publish().RefCount().AsObservable();
         }
 
         public Task PublishAsync(TMessage message)
@@ -29,9 +34,6 @@ namespace Obvs
             _subject.Dispose();
         }
 
-        public IObservable<TMessage> Messages
-        {
-            get { return _subject.AsObservable(); }
-        }
+        public IObservable<TMessage> Messages { get; private set; }
     }
 }
