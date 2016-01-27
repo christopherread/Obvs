8ef85333a6c93a362bdd1537e4f1a629c85912ed
James Connor <james.connor@starlizard.com>
2016-01-27 10:39:48 +0000

    Changed Serializer/Deserializer interface to aid future usages

--- Obvs/Serialization/IMessageSerializer.cs
+++ Obvs/Serialization/IMessageSerializer.cs
@@ -2,9 +2,21 @@ using System.IO;
 
 namespace Obvs.Serialization
 {
+    public static class MessageSerializerExtentions
+    {
+        public static byte[] Serialize(this IMessageSerializer serializer, object obj)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                serializer.Serialize(ms, obj);
+
+                return ms.ToArray();
+            }
+        }
+    }
+
     public interface IMessageSerializer
     {
-        object Serialize(object message);
         void Serialize(Stream destination, object message);
     }
 }
