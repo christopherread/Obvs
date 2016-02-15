c6a1ce063283c6f5721e04ac1f2a7b8a59ac485a
Christopher Read <christopher.read@starlizard.com>
2016-02-15 13:07:39 +0000

    Updated to Obvs 3.0 and latest Nest package

--- Obvs.Monitoring.ElasticSearch/ObvsCounter.cs
+++ Obvs.Monitoring.ElasticSearch/ObvsCounter.cs
@@ -3,17 +3,13 @@ using Nest;
 
 namespace Obvs.Monitoring.ElasticSearch
 {
-    [ElasticType(Name = "obvscounter")]
     public class ObvsCounter
     {
         public string Id { get; set; }
         public DateTime TimeStamp { get; set; }
         public DateTime TimeStampUTC { get; set; }
-        [ElasticProperty(OmitNorms = true, Index = FieldIndexOption.NotAnalyzed)]
         public string CounterName { get; set; }
-        [ElasticProperty(OmitNorms = true, Index = FieldIndexOption.NotAnalyzed)]
         public string InstanceName { get; set; }
-        [ElasticProperty(OmitNorms = true, Index = FieldIndexOption.NotAnalyzed)]
         public string MessageType { get; set; }
         public string Direction { get; set; }
         public int Count { get; set; }
@@ -23,9 +19,7 @@ namespace Obvs.Monitoring.ElasticSearch
         public double MinElapsedMs { get; set; }
         public double TotalElapsedMs { get; set; }
         public string UserName { get; set; }
-        [ElasticProperty(OmitNorms = true, Index = FieldIndexOption.NotAnalyzed)]
         public string HostName { get; set; }
-        [ElasticProperty(OmitNorms = true, Index = FieldIndexOption.NotAnalyzed)]
         public string ProcessName { get; set; }
 
         public ObvsCounter()
