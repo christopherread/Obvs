82a8a90673b5c48089df0678d7711f801f578f07
James Connor <james.connor@starlizard.com>
2017-10-04 17:37:13 +0100

    Adding MessagePackCSharpMessageDeserializer constructor

--- Obvs.Serialization.MessagePack-CSharp/MessagePackCSharpMessageDeserializer.cs
+++ Obvs.Serialization.MessagePack-CSharp/MessagePackCSharpMessageDeserializer.cs
@@ -6,9 +6,16 @@ namespace Obvs.Serialization.MessagePack
     public class MessagePackCSharpMessageDeserializer<TMessage> : MessageDeserializerBase<TMessage> 
         where TMessage : class
     {
+        private readonly IFormatterResolver _resolver;
+
+        public MessagePackCSharpMessageDeserializer(IFormatterResolver resolver)
+        {
+            _resolver = resolver;
+        }
+
         public override TMessage Deserialize(Stream source)
         {
-            return MessagePackSerializer.Deserialize<TMessage>(source);
+            return MessagePackSerializer.Deserialize<TMessage>(source, _resolver);
         }
     }
 }
